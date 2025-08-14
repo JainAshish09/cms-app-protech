e18d1fd5ce5c65cf32e104df6daa49446a2c5e50
Ashish Jain <ashish.jain09@nagarro.com>
2025-08-14 06:54:54 +0530

    Logo Updated

--- components/widgets/logo.tsx
+++ components/widgets/logo.tsx
@@ -1,10 +1,8 @@
-// components/Logo.js (or .jsx)
-
 import React from 'react';
 
 export default function Logo() {
     return (
-        <div className="flex flex-1 justify-center items-center">
+        <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <div className="flex items-baseline space-x-2 text-center md:text-left">
                 <span className="text-black text-xl md:text-2xl font-extrabold tracking-tight">ASSA ABLOY</span>
                 <span className="text-black text-xs md:text-sm font-normal tracking-wide">PRO-TECH TITAN®</span>
